52591c7805a6081315a1ee1615adfb8a02cc6b1c
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-12-12 06:11:43 +0330

    fix: dark & light favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import '@/styles/main.scss'
 
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 import type { ReactElement, ReactNode } from 'react'
 
 import Head from 'next/head'
@@ -21,10 +22,23 @@ interface AppPropsWithLayout extends AppProps {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.layout ?? ((page) => page)
+  const [isDark, setIsDark] = useState<boolean>()
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
+    prefersDark.addEventListener('change', (e) => setIsDark(e.matches))
+    setIsDark(prefersDark.matches)
+  }, [])
+
   return (
     <>
       <Head>
         <SEOMetaTags />
+        {isDark ? (
+          <link rel="icon" href="/favicon-dark.ico" />
+        ) : (
+          <link rel="icon" href="/favicon.ico" />
+        )}
       </Head>
 
       {Layout(<Component {...pageProps} />)}
