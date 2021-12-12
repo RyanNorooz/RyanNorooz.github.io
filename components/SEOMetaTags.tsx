92f5be9cd0e9561503ef5e6de11dba67b88f5aa4
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-12-12 06:10:35 +0330

    fix: add more meta tags

--- components/SEOMetaTags.tsx
+++ components/SEOMetaTags.tsx
@@ -4,22 +4,30 @@ export default function SEOMetaTags() {
     <>
       {/* Primary Meta Tags */}
       <title>Ryan Norooz - Web Designer & Developer</title>
-      <meta name="title" content="Ryan Norooz - Web Designer & Developer"/>
-      <meta name="description" content="I can design and develop your next PWA app. I specialize in front-end and frameworks like React and Vue.js"/>
+      <meta name="title" content="Ryan Norooz - Web Designer & Developer" />
+      <meta name="description" content="I can design and develop your next PWA app. I specialize in front-end and frameworks like React and Vue.js" />
 
       {/* Open Graph / Facebook */}
-      <meta property="og:type" content="website"/>
-      <meta property="og:url" content="https://metatags.io/"/>
-      <meta property="og:title" content="Ryan Norooz - Web Designer & Developer"/>
-      <meta property="og:description" content="I can design and develop your next PWA app. I specialize in front-end and frameworks like React and Vue.js"/>
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://ryannorooz.github.io/" />
+      <meta property="og:title" content="Ryan Norooz - Web Designer & Developer" />
+      <meta property="og:description" content="I can design and develop your next PWA app. I specialize in front-end and frameworks like React and Vue.js" />
+      <meta property="og:image" content="/link-preview.png" />
 
       {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="https://metatags.io/"/>
-      <meta property="twitter:title" content="Ryan Norooz - Web Designer & Developer"/>
-      <meta property="twitter:description" content="I can design and develop your next PWA app. I specialize in front-end and frameworks like React and Vue.js"/>
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://ryannorooz.github.io/" />
+      <meta property="twitter:title" content="Ryan Norooz - Web Designer & Developer" />
+      <meta property="twitter:description" content="I can design and develop your next PWA app. I specialize in front-end and frameworks like React and Vue.js" />
+      <meta property="twitter:image" content="/link-preview.png" />
+
+      {/* Search Engines */}
+      <meta name="author" content="Ryan Norooz" />
+      <meta name="keywords" content="developer, frontend, react, vue, web, portfolio" />
+      <meta name="robots" content="index, follow" />
+      <meta name="revisit-after" content="14 days" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
     </>
   )
 }
