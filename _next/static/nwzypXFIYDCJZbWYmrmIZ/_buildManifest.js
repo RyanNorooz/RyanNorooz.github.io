9c2cb62535e86d326b28b67b828fb8cc93d64e0e
RyanNorooz <58340667+RyanNorooz@users.noreply.github.com>
2021-12-13 02:57:47 +0000

    Deploying to gh-pages from @ RyanNorooz/ryannorooz.github.io@ff1c2bea258181aed294595e23b377b91df553f8 🚀

--- _next/static/nwzypXFIYDCJZbWYmrmIZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/23-a8101bc3714295ec.js","static/chunks/pages/index-ad1a69eeb5fa29df.js"],"/_error":["static/chunks/pages/_error-c96163b5243a5b97.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
