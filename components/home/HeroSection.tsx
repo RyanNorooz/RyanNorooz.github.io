884ef6b5e65381756fba079ac0b7c755a8b8e6bc
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-12-13 21:06:14 +0330

    fix: change hero section styles

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -3,6 +3,17 @@ import { Icon } from '@iconify/react'
 import stickDudeArt from '@/public/img/stick-dude.png'
 
 export default function HeroSection() {
+  const quotes = [
+    'Thank you for invading my space.',
+    "One day i'm gonna make the onions cry.",
+    'I am the one who knocks.',
+    'They said dont give up on your dreams. so i went back to sleep',
+    'Im always lazy, so I became a programmer',
+    "I'm not a great programmer, I'm just a good programmer with great habits.",
+    'The best way to predict the future is to create it.',
+    '“The best way to predict the future is to invent it.”',
+    "If you are a programmer, you can't do anything until you can do something else.",
+  ]
   return (
     <section className="home" id="home">
       <div className="home__container bd-grid">
@@ -13,7 +24,7 @@ export default function HeroSection() {
         </h1>
 
         <p className="pl-2 home__subtitle font-semibold">
-          Thank you for invading my space.
+          {quotes[Math.floor(Math.random() * quotes.length)]}
         </p>
 
         <div className="home__scroll">
