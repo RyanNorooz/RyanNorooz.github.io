3a92289ddf38c81dc78a0f73e0c5794f76573c85
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-12-14 01:08:57 +0330

    feat(hero section): over engineer a simple gimmick

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,19 +1,46 @@
 import Image from 'next/image'
 import { Icon } from '@iconify/react'
+import { useEffect, useState } from 'react'
 import stickDudeArt from '@/public/img/stick-dude.png'
+import {
+  setLocalStorageWithExpiry,
+  getLocalStorageWithExpiry,
+} from '@/lib/logic'
+
+const quotes = [
+  'Thank you for invading my space.',
+  'I make stuff.',
+  'I break stuff.',
+  "I'm so good at breaking stuff.",
+  'I am a stick figure.',
+  "Knock, knock. Who's there?",
+  "IT'S A ME MARIO!",
+  'Im so lazy I became a programmer.',
+  "They said don't give up on your dreams, so I went back to sleep.",
+  "I'm not a great programmer, I'm just a good programmer with great habits.",
+  "If you are a programmer, you can't do anything until you can do something else.",
+  "You're still here?",
+  'Stop refreshing for fuck sake.',
+  'Seriously, stop.',
+  "There's nothin ahead.",
+  'I lied.',
+  'There is something ahead.',
+  'Stay tuned for the grand reveal.',
+]
 
 export default function HeroSection() {
-  const quotes = [
-    'Thank you for invading my space.',
-    "One day i'm gonna make the onions cry.",
-    'I am the one who knocks.',
-    'They said dont give up on your dreams. so i went back to sleep',
-    'Im always lazy, so I became a programmer',
-    "I'm not a great programmer, I'm just a good programmer with great habits.",
-    'The best way to predict the future is to create it.',
-    '“The best way to predict the future is to invent it.”',
-    "If you are a programmer, you can't do anything until you can do something else.",
-  ]
+  const [theQuote, setQuote] = useState<string>()
+
+  // Get the quote index from local storage
+  // get function checks if value is expired
+  useEffect(() => {
+    const lastIndex = parseInt(getLocalStorageWithExpiry('quoteIndex') || 0)
+    setQuote(quotes[lastIndex])
+    const nextIndex = lastIndex + 1 >= quotes.length ? 0 : lastIndex + 1
+    // expire 5 minutes after last refresh
+    setLocalStorageWithExpiry('quoteIndex', nextIndex.toString(), 300000)
+  }, [])
+
   return (
     <section className="home" id="home">
       <div className="home__container bd-grid">
@@ -23,8 +50,8 @@ export default function HeroSection() {
           LLO.
         </h1>
 
-        <p className="pl-2 home__subtitle font-semibold">
-          {quotes[Math.floor(Math.random() * quotes.length)]}
+        <p className="pl-2 home__subtitle font-semibold max-w-[35ch]">
+          {theQuote}
         </p>
 
         <div className="home__scroll">
