f7b9033899ff3bfb8a96da1dafadc2a7795293b1
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2022-01-04 04:49:27 +0330

    fix: add localized seo tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,34 @@
+// ################### Styles ###################
 import 'tailwindcss/tailwind.css'
-
 import '@/public/fonts/IRANSansX/IRANSansX.css'
 import '@/styles/styles.scss'
 import '@/styles/main.scss'
 
+// ################### Types ###################
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import type { ReactElement, ReactNode } from 'react'
 
+// ########## Libraries and Components ##########
+import { appWithTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import SEOMetaTags from '@/components/SEOMetaTags'
+import SEOMetaTagsFA from '@/components/SEOMetaTagsFA'
 
 type NextPageWithLayout = NextPage & {
   layout?: (page: ReactElement) => ReactNode
 }
-
 interface AppPropsWithLayout extends AppProps {
   Component: NextPageWithLayout
 }
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
   const Layout = Component.layout ?? ((page) => page)
+
+  // the whole purpose of isDark is that the proper logo shade is shown
   const [isDark, setIsDark] = useState<boolean>()
 
   useEffect(() => {
@@ -33,15 +40,13 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <>
       <Head>
-        <SEOMetaTags />
-        {isDark ? (
-          <link rel="icon" href="/favicon-dark.ico" />
-        ) : (
-          <link rel="icon" href="/favicon.ico" />
-        )}
+        {router.locale === 'fa' ? SEOMetaTagsFA : SEOMetaTags}
+        <link rel="icon" href={isDark ? '/favicon-dark.ico' : '/favicon.ico'} />
       </Head>
 
       {Layout(<Component {...pageProps} />)}
     </>
   )
 }
+
+export default appWithTranslation(MyApp)
